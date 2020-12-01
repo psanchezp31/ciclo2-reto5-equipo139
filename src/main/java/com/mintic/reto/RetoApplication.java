632dfa02d49a160bad8c48bb6ed0b27eb8b91e97
Paula Sanchez <paula.sanchez1031@gmail.com>
2020-12-01 16:34:36 -0500

    Eliminando el ComponentScan

--- src/main/java/com/mintic/reto/RetoApplication.java
+++ src/main/java/com/mintic/reto/RetoApplication.java
@@ -3,12 +3,12 @@ package com.mintic.reto;
 import com.mintic.reto.gui.PrincipalFrame;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.mintic.reto")
 public class RetoApplication implements CommandLineRunner {
 
     @Autowired
